90c4235d5004326a9e6315ab85324d70772f64e1
Arifdwi332 <ariefdwi332@gmail.com>
2025-08-09 19:53:52 +0700

    first commit

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,19 @@
+@extends('templates.layout')
+
+@section('breadcrumbs')
+    Dashboard
+@endsection
+
+@section('content')
+    <div class="card">
+        <div class="card-header">
+            test
+        </div>
+        <div class="card-body">
+            test
+        </div>
+        <div class="card-footer">
+            <button class="btn btn-primary">test</button>
+        </div>
+    </div>
+@endsection
